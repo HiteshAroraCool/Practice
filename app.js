70b26c5fc11e0eaac75ba13ec2e37798b9effd95
Hitesh Arora <76745725+HiteshAroraCool@users.noreply.github.com>
2024-03-09 12:30:22 +0000

    Following: https://www.youtube.com/watch?v=jS4aFq5-91M&t=1328s

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+let welcomeEl = document.getElementById("welcomeEl")
+
+let name_ = "Name"
+let greet = " Welcome back!!!!"
+
+welcomeEl.innerText = name_ + greet
+
 function increment () {
-    console.log("Botton was click")
-  }
+  count += 1
+  countEl.textContent = count
+}
+
+function save() {
+  let message = count + " - "
+  saveEl.textContent += message
+  count = 0
+  countEl.textContent = count
+}
+
